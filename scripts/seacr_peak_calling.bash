d545b549a1348069f476bc870fc3ad33b0f9ec47
mmahlke <69474772+mmahlke@users.noreply.github.com>
2025-04-08 13:14:08 -0400

    Update seacr_peak_calling.bash

--- scripts/seacr_peak_calling.bash
+++ scripts/seacr_peak_calling.bash
@@ -35,28 +35,28 @@ SEACR_1.3.sh PDNC4_test_seacr.bedgraph 0.01 non relaxed PDNC4_test_0.01
 
 ##CA-HJ-LAP_cC4
 #With a - control
-SEACR_1.3.sh CA-HJ-LAP_cC4_seacr.bedgraph Neg_control_seacr.bedgraph norm stringent PDNC4_test_ctrl 
-SEACR_1.3.sh CA-HJ-LAP_cC4_seacr.bedgraph Neg_control_seacr.bedgraph norm relaxed PDNC4_test_ctrl
+SEACR_1.3.sh CA-HJ-LAP_cC4_seacr.bedgraph Neg_control_seacr.bedgraph norm stringent CA-HJ-LAP_cC4 
+SEACR_1.3.sh CA-HJ-LAP_cC4_seacr.bedgraph Neg_control_seacr.bedgraph norm relaxed CA-HJ-LAP_cC4
 
 #Without a - control
-SEACR_1.3.sh CA-HJ-LAP_cC4_seacr.bedgraph 0.00001 non stringent PDNC4_test_0.00001
-SEACR_1.3.sh CA-HJ-LAP_cC4_seacr.bedgraph 0.00001 non relaxed PDNC4_test_0.00001
-SEACR_1.3.sh CA-HJ-LAP_cC4_seacr.bedgraph 0.01 non stringent PDNC4_test_0.01
-SEACR_1.3.sh CA-HJ-LAP_cC4_seacr.bedgraph 0.01 non relaxed PDNC4_test_0.01
+SEACR_1.3.sh CA-HJ-LAP_cC4_seacr.bedgraph 0.00001 non stringent CA-HJ-LAP_cC4_0.00001
+SEACR_1.3.sh CA-HJ-LAP_cC4_seacr.bedgraph 0.00001 non relaxed CA-HJ-LAP_cC4_0.00001
+SEACR_1.3.sh CA-HJ-LAP_cC4_seacr.bedgraph 0.01 non stringent CA-HJ-LAP_cC4_0.01
+SEACR_1.3.sh CA-HJ-LAP_cC4_seacr.bedgraph 0.01 non relaxed CA-HJ-LAP_cC4_0.01
 
 
 ##E2_12m_sc_D4
 #With a - control
-SEACR_1.3.sh E2_12m_sc_D4_seacr.bedgraph Neg_control_seacr.bedgraph norm stringent PDNC4_test_ctrl 
-SEACR_1.3.sh E2_12m_sc_D4_seacr.bedgraph Neg_control_seacr.bedgraph norm relaxed PDNC4_test_ctrl
+SEACR_1.3.sh E2_12m_sc_D4_seacr.bedgraph Neg_control_seacr.bedgraph norm stringent E2_12m_sc_D4 
+SEACR_1.3.sh E2_12m_sc_D4_seacr.bedgraph Neg_control_seacr.bedgraph norm relaxed E2_12m_sc_D4
 
 #Without a - control
-SEACR_1.3.sh E2_12m_sc_D4_seacr.bedgraph 0.00001 non stringent PDNC4_test_0.00001
-SEACR_1.3.sh E2_12m_sc_D4_seacr.bedgraph 0.00001 non relaxed PDNC4_test_0.00001
-SEACR_1.3.sh E2_12m_sc_D4_seacr.bedgraph 0.01 non stringent PDNC4_test_0.01
-SEACR_1.3.sh E2_12m_sc_D4_seacr.bedgraph 0.01 non relaxed PDNC4_test_0.01
+SEACR_1.3.sh E2_12m_sc_D4_seacr.bedgraph 0.00001 non stringent E2_12m_sc_D4_0.00001
+SEACR_1.3.sh E2_12m_sc_D4_seacr.bedgraph 0.00001 non relaxed E2_12m_sc_D4_0.00001
+SEACR_1.3.sh E2_12m_sc_D4_seacr.bedgraph 0.01 non stringent E2_12m_sc_D4_0.01
+SEACR_1.3.sh E2_12m_sc_D4_seacr.bedgraph 0.01 non relaxed E2_12m_sc_D4_0.01
 
 
 #Move these new peak files to ./seacr_peaks
-mv *stringent.bed > ./seacr_peaks
-mv *relaxed.bed > ./seacr_peaks
+mv *stringent.bed > ./seacr_peaks/
+mv *relaxed.bed > ./seacr_peaks/
